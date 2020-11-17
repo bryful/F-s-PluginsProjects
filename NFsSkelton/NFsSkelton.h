b12553fe4d6c30680e4f93d1d06cb52c8c086a59
bryful <bryful@gmail.com>
2020-11-17 22:41:47 +0900

    NFsSkelton

--- NFsSkelton/NFsSkelton.h
+++ NFsSkelton/NFsSkelton.h
@@ -10,6 +10,7 @@
 
 #include "../NFsLibrary/AE_SDK.h"
 #include "../NFsLibrary/AEInfo.h"
+#include "../NFsLibrary/NFWorld.h"
 
 
 #include "NFs_Target.h"
@@ -20,7 +21,6 @@
 //ParamsSetup関数とRender関数のparamsパラメータのIDになる
 enum {
 	ID_INPUT = 0,	// default input layer
-
 	ID_VALUE,
 	ID_CHECK,
 	ID_NUM_PARAMS
@@ -44,31 +44,14 @@ typedef struct ParamInfo {
 class NFsSkelton : public AEInfo
 {
 public:
-	NFsSkelton() 
-	{
-	};
-
-	NFsSkelton(
-		PF_Cmd			cmd,
-		PF_InData*		in_dataP,
-		PF_OutData*		out_data,
-		PF_ParamDef*	params[],
-		PF_LayerDef*	output,
-		void*			extraP
-	) :AEInfo(
-		cmd,
-		in_dataP,
-		out_data,
-		params,
-		output,
-		extraP,
-		ID_NUM_PARAMS,
-		sizeof(ParamInfo)
-	)
-	{
-
-	};
-	
+	// ******************************************************
+	PF_Err GetParams(ParamInfo *infoP);
+	PF_Err Exec(ParamInfo* infoP);
+	PF_Err ParamsSetup(
+		PF_InData* in_dataP,
+		PF_OutData* out_dataP,
+		PF_ParamDef* paramsP[],
+		PF_LayerDef* outputP) override;
 	// ******************************************************
 	PF_Err	About(
 		PF_InData* in_dataP,
@@ -77,12 +60,21 @@ public:
 		PF_LayerDef* outputP) override
 	{
 		PF_Err err = PF_Err_NONE;
-		AEInfo::About(in_dataP, out_dataP, paramsP, outputP);
+		Init();
+		m_cmd = PF_Cmd_ABOUT;
+		in_data = in_dataP;
+		GetFrame(in_dataP);
+		GetSuites(in_dataP);
+
+		A_char MJV[64] = { '\0' };
+		A_char MNV[64] = { '\0' };
+		PF_SPRINTF(MJV, "%d", MAJOR_VERSION);
+		PF_SPRINTF(MNV, "%d", MINOR_VERSION);
 
 		ERR(AboutBox(
 			NFS_DISPNAME,
-			MAJOR_VERSION,
-			MINOR_VERSION,
+			MJV,
+			MNV,
 			NFS_DESCRIPTION));
 
 		return err;
@@ -93,27 +85,62 @@ public:
 		PF_InData* in_dataP,
 		PF_OutData* out_dataP,
 		PF_ParamDef* paramsP[],
-		PF_LayerDef* outputP)
+		PF_LayerDef* outputP) override
 	{
 		PF_Err	err = PF_Err_NONE;
 		Init();
+		m_cmd = PF_Cmd_GLOBAL_SETUP;
 		in_data = in_dataP;
 		suitesP = new AEGP_SuiteHandler(in_dataP->pica_basicP);
 
+		//Fs_Target.hで定義
+		out_data->my_version = NFS_VERSION;
+		out_data->out_flags = NFS_OUT_FLAGS;
+		out_data->out_flags2 = NFS_OUT_FLAGS2;
+		/*
+		プラグインIDを獲得して、グローバルにアクセスできるように保存
+		*/
+		ae_plugin_idH = suitesP->HandleSuite1()->host_new_handle(sizeof(ae_global_data));
+
+		if (ae_plugin_idH) {
+			ae_plugin_idP = reinterpret_cast<ae_global_dataP>(suitesP->HandleSuite1()->host_lock_handle(ae_plugin_idH));
+			if (ae_plugin_idP) {
+				ae_plugin_idP->initializedB = TRUE;
+
+				if (in_data->appl_id != 'PrMr') {
+					ERR(suitesP->UtilitySuite3()->AEGP_RegisterWithAEGP(NULL, NFS_NAME, &ae_plugin_idP->my_id));
+				}
+				if (!err) {
+					out_data->global_data = ae_plugin_idH;
+				}
+			}
+			suitesP->HandleSuite1()->host_unlock_handle(ae_plugin_idH);
+		}
+		else {
+			err = PF_Err_INTERNAL_STRUCT_DAMAGED;
+		}
 
 		return err;
 	}
 	// ******************************************************
-	PF_Err ParamsSetup(
-			PF_InData * in_data,
-			PF_OutData * out_data,
-			PF_ParamDef * params[],
-			PF_LayerDef * output) override
+	PF_Err GlobalSetdown(PF_InData* in_dataP) override
 	{
-		PF_Err err = PF_Err_NONE;
+		PF_Err	err = PF_Err_NONE;
+		Init();
+		m_cmd = PF_Cmd_GLOBAL_SETDOWN;
+		suitesP = new AEGP_SuiteHandler(in_dataP->pica_basicP);
+		in_data = in_dataP;
+
+		//ae_plugin_idH
+		if (in_dataP->global_data) {
+			suitesP->HandleSuite1()->host_dispose_handle(in_dataP->global_data);
+		}
 
 		return err;
-	};
+	}
+	// ******************************************************
+	
+
 };
 
 //-----------------------------------------------------------------------------------
@@ -130,6 +157,7 @@ extern "C" {
 			void*			extraP)
 	{
 		PF_Err err = PF_Err_NONE;
+		PF_Err err2 = PF_Err_NONE;
 
 		try
 		{
@@ -158,13 +186,39 @@ extern "C" {
 				err = ae.SequenceResetup(in_dataP, out_dataP, paramsP, outputP);
 				break;
 			case PF_Cmd_RENDER:
-				err = ae.Render(in_dataP, out_dataP, paramsP, outputP);
+				err = ae.Render(in_dataP, out_dataP, paramsP, outputP,ID_NUM_PARAMS);
+				if (!err)
+				{
+					ParamInfo info;
+					ERR(ae.GetParams(&info));
+					ERR(ae.Exec(&info));
+				}
 				break;
 			case PF_Cmd_SMART_PRE_RENDER:
-				err = ae.PreRender(in_dataP, out_dataP, reinterpret_cast<PF_PreRenderExtra*>(extraP));
+				err = ae.PreRender(in_dataP, out_dataP, reinterpret_cast<PF_PreRenderExtra*>(extraP),ID_NUM_PARAMS,sizeof(ParamInfo));
+				if (!err)
+				{
+					ParamInfo* infoP = reinterpret_cast<ParamInfo*>(ae.LockPreRenderData());
+					ERR(ae.SetHostPreRenderData());
+					ERR(ae.GetParams(infoP));
+					ERR(ae.UnSetPreRenderData());
+					ERR(ae.UnlockPreRenderData());
+
+				}
 				break;
 			case PF_Cmd_SMART_RENDER:
-				err = ae.SmartRender(in_dataP, out_dataP, reinterpret_cast<PF_SmartRenderExtra*>(extraP));
+				err = ae.SmartRender(in_dataP, out_dataP, reinterpret_cast<PF_SmartRenderExtra*>(extraP),ID_NUM_PARAMS);
+				if (!err) {
+					ParamInfo* infoP = reinterpret_cast<ParamInfo*>(ae.LockPreRenderData());
+					if (infoP) {
+						ERR(ae.Exec(infoP));
+						ERR2(ae.UnsetSmartRender());
+						ae.UnlockPreRenderData();
+					}
+					else {
+						err = PF_Err_OUT_OF_MEMORY;
+					}
+				}
 				break;
 			case PF_Cmd_COMPLETELY_GENERAL:
 				err = ae.RespondtoAEGP(in_dataP, out_dataP, paramsP, outputP, extraP);
