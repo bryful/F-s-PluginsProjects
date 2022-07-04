df97f48e815af8ef127b5c478c450c7ccb80b250
bryful <bryful@gmail.com>
2022-07-04 09:59:17 +0900

    bugfix EdgeLine

--- EdgeLine/EdgeLine.cpp
+++ EdgeLine/EdgeLine.cpp
@@ -109,7 +109,7 @@ static PF_Err GetParams(CFsAE *ae, ParamInfo8 *infoP)
 	PF_FpLong v;
 	ERR(ae->GetFLOAT(ID_LEVEL,&v));
 	if(!err){
-		infoP->level  = (A_u_char)(PF_MAX_CHAN8 * v);
+		infoP->level  = (A_u_char)((double)PF_MAX_CHAN8 * v/100);
 	}
 	ERR(ae->GetADD(ID_LENGTH,&infoP->length));
 	if (infoP->length>0){
@@ -123,12 +123,15 @@ static PF_Err GetParams(CFsAE *ae, ParamInfo8 *infoP)
 static ParamInfo16 convTo16(ParamInfo8 *info)
 {
 	ParamInfo16 ret;
+	AEFX_CLR_STRUCT(ret);
 	ret.targetCol	= info->targetCol;
 	ret.sampleCol	= info->sampleCol;
 	ret.level		= info->level;
 	ret.length		= info->length;
 	ret.drawCol		= CONV8TO16(info->drawCol);
 	ret.w			= info->w;
+	ret.h			= info->h;
+
 	ret.ae			= info->ae;
 	return ret;
 }
@@ -136,12 +139,14 @@ static ParamInfo16 convTo16(ParamInfo8 *info)
 static ParamInfo32 convTo32(ParamInfo8 *info)
 {
 	ParamInfo32 ret;
+	AEFX_CLR_STRUCT(ret);
 	ret.targetCol	= info->targetCol;
 	ret.sampleCol	= info->sampleCol;
 	ret.level		= info->level;
 	ret.length		= info->length;
 	ret.drawCol		= CONV8TO32(info->drawCol);
 	ret.w			= info->w;
+	ret.h = info->h;
 	ret.ae			= info->ae;
 	return ret;
 }
@@ -190,7 +195,8 @@ FilterImage16 (
 {
 	PF_Err			err = PF_Err_NONE;
 	ParamInfo16 *	niP		= reinterpret_cast<ParamInfo16*>(refcon);
-	if (compPix16_8Lv(*inP,niP->targetCol,niP->level)==FALSE) return err;
+	PF_Pixel t = CONV16TO8(*inP);
+	if (compPix8Lv(t,niP->targetCol,niP->level)==FALSE) return err;
 
 	int x0 = xL - niP->length;
 	if (x0<0) x0=0;
@@ -203,7 +209,8 @@ FilterImage16 (
 
 	for ( int y = y0; y<=y1; y++){
 		for ( int x = x0; x<=x1; x++){
-			if (compPix16_8Lv(niP->ae->in->getPixel16(x,y),niP->sampleCol,niP->level)==TRUE) {
+			PF_Pixel t2 = CONV16TO8(niP->ae->in->getPixel16(x, y));
+			if (compPix8Lv(t2,niP->sampleCol,niP->level)==TRUE) {
 				*outP = niP->drawCol;
 				break;
 			}
