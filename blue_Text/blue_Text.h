3cb08ab2754a1ea274ca1146c574f41df54df5d6
古橋 宏 <bryful@gmail.com>
2026-01-13 22:38:38 +0900

    blue_Textプラグイン新規追加と関連ファイルの導入
    After Effects用エフェクトプラグイン「blue_Text」を新規追加。プロジェクトファイル、PiPLリソース、ビルド設定、サンプル、共通ユーティリティ、ボックスブラー(TinyBlue)の実装など、開発・ビルドに必要な一式を追加。今後の拡張を見据えた構成。

---
+++ blue_Text/blue_Text.h
@@ -0,0 +1,53 @@
+﻿//-----------------------------------------------------------------------------------
+/*
+	blue_Text for VS2010
+*/
+//-----------------------------------------------------------------------------------
+
+#pragma once
+#ifndef blue_Text_H
+#define blue_Text_H
+
+#include "../FsLibrary/Fs.h"
+#include "Fs_Target.h"
+#include "../FsLibrary/FsAE.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include "TinyBlue.h"
+
+//ユーザーインターフェースのID
+//ParamsSetup関数とRender関数のparamsパラメータのIDになる
+enum {
+	ID_INPUT = 0,	// default input layer
+
+	ID_RADIUS,
+
+	ID_NUM_PARAMS
+};
+
+
+//UIの表示文字列
+#define	STR_RADIUS			"radius"
+//UIのパラメータ
+typedef struct {
+	A_long				radius;
+} ParamInfo;
+
+
+//-----------------------------------------------------------------------------------
+extern "C" {
+
+DllExport 
+PF_Err 
+EffectMain(
+	PF_Cmd			cmd,
+	PF_InData		*in_data,
+	PF_OutData		*out_data,
+	PF_ParamDef		*params[],
+	PF_LayerDef		*output,
+	void			*extra);
+}
+#endif // blue_Text_H
